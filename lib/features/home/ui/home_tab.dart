a939cefec824e9e2001dd5224ee567c132bf1b1e
danaa-zh <danazholdybai4@gmail.com>
2026-04-30 09:47:42 +0500

    home tab исправления

--- lib/features/home/ui/home_tab.dart
+++ lib/features/home/ui/home_tab.dart
@@ -3,14 +3,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 
+import 'package:hygge_app/core/constants/app_constants.dart';
 import 'package:hygge_app/core/constants/app_paddings.dart';
+import 'package:hygge_app/core/constants/app_spacings.dart';
 import 'package:hygge_app/core/constants/asset_paths.dart';
 import 'package:hygge_app/core/theme/app_text_styles.dart';
 import 'package:hygge_app/features/home/bloc/home_bloc.dart';
 import 'package:hygge_app/features/home/bloc/home_event.dart';
 import 'package:hygge_app/features/home/bloc/home_state.dart';
-import 'package:hygge_app/features/notifications/bloc/notifications_cubit.dart';
-import 'package:hygge_app/features/notifications/bloc/notifications_state.dart';
 import 'package:hygge_app/features/programs_list/ui/programm_card.dart';
 import 'package:hygge_app/features/programs_list/ui/programm_list.dart';
 import 'package:hygge_app/widgets/tab_header.dart';
@@ -51,19 +51,21 @@ class _MainTabView extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const ProgramsHeader(
-                  trailing: _NotificationsBell(),
+                ProgramsHeader(
+                  trailing: _notificationIconButton(),
                 ),
                 Expanded(
                   child: SingleChildScrollView(
-                    padding: const EdgeInsets.only(bottom: 24),
+                    padding: const EdgeInsets.only(
+                      bottom: AppConstants.programsCardsBottomInset,
+                    ),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Padding(
                           padding: const EdgeInsets.only(
                             left: AppPaddings.programsScreenHorizontal,
-                            top: 8,
+                            top: AppSpacings.sm,
                             right: AppPaddings.programsScreenHorizontal,
                           ),
                           child: RichText(
@@ -84,7 +86,7 @@ class _MainTabView extends StatelessWidget {
                             ),
                           ),
                         ),
-                        const SizedBox(height: 24),
+                        const SizedBox(height: AppSpacings.xl),
                         Padding(
                           padding: const EdgeInsets.only(
                             left: AppPaddings.programsScreenHorizontal,
@@ -96,11 +98,13 @@ class _MainTabView extends StatelessWidget {
                             ),
                           ),
                         ),
-                        const SizedBox(height: 12),
+                        const SizedBox(height: AppSpacings.scheduleSignedTitleGap),
                         Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: AppSpacings.md,
+                          ),
                           child: ClipRRect(
-                            borderRadius: BorderRadius.circular(35),
+                            borderRadius: BorderRadius.circular(AppConstants.programsCardRadius),
                             child: Stack(
                               children: [
                                 Image.asset(
@@ -110,8 +114,8 @@ class _MainTabView extends StatelessWidget {
                                   fit: BoxFit.cover,
                                 ),
                                 Positioned(
-                                  left: 20,
-                                  bottom: 16,
+                                  left: AppSpacings.xl,
+                                  bottom: AppSpacings.lg,
                                   child: Text(
                                     loc.readOurNews,
                                     style: AppTextStyles.programsFilter,
@@ -121,7 +125,7 @@ class _MainTabView extends StatelessWidget {
                             ),
                           ),
                         ),
-                        const SizedBox(height: 28),
+                        const SizedBox(height: AppSpacings.programsBodyGap),
                         Padding(
                           padding: const EdgeInsets.only(
                             left: AppPaddings.programsScreenHorizontal,
@@ -134,7 +138,7 @@ class _MainTabView extends StatelessWidget {
                             ),
                           ),
                         ),
-                        const SizedBox(height: 16),
+                        const SizedBox(height: AppSpacings.lg),
                         const _UpcomingProgramsList(),
                       ],
                     ),
@@ -158,7 +162,7 @@ class _UpcomingProgramsList extends StatelessWidget {
       builder: (context, state) {
         if (state.isLoading) {
           return const Padding(
-            padding: EdgeInsets.all(24),
+            padding: EdgeInsets.all(AppSpacings.xl),
             child: Center(
               child: CircularProgressIndicator(),
             ),
@@ -169,10 +173,10 @@ class _UpcomingProgramsList extends StatelessWidget {
           return Padding(
             padding: const EdgeInsets.symmetric(
               horizontal: AppPaddings.programsScreenHorizontal,
-              vertical: 12,
+              vertical: AppSpacings.scheduleSignedTitleGap,
             ),
             child: Text(
-              _emptyText(context),
+              context.read<AppLocalizations>().noUpcomingPrograms,
               style: AppTextStyles.programsSubtitle,
             ),
           );
@@ -189,7 +193,7 @@ class _UpcomingProgramsList extends StatelessWidget {
                 final lesson = state.lessons[index];
 
                 return Padding(
-                  padding: const EdgeInsets.only(bottom: 16),
+                  padding: const EdgeInsets.only(bottom: AppSpacings.lg),
                   child: ProgrammCard(
                     type: ProgrammCardType.big,
                     lesson: lesson,
@@ -203,74 +207,20 @@ class _UpcomingProgramsList extends StatelessWidget {
     );
   }
 
-  static String _emptyText(BuildContext context) {
-    final code = Localizations.localeOf(context).languageCode;
 
-    return switch (code) {
-      'en' => 'No upcoming programs yet',
-      'kk' => 'Әзірге алдағы бағдарламалар жоқ',
-      _ => 'Пока нет предстоящих программ',
-    };
-  }
-}
-
-class _NotificationsBell extends StatelessWidget {
-  const _NotificationsBell();
-
-  @override
-  Widget build(BuildContext context) {
-    final cubit = context.read<NotificationsCubit?>();
-
-    if (cubit == null) {
-      return IconButton(
-        onPressed: () => context.go('/home/notifications'),
-        icon: const _NotificationSvgIcon(),
-      );
-    }
-
-    return BlocBuilder<NotificationsCubit, NotificationsState>(
-      bloc: cubit,
-      builder: (context, state) {
-        return Stack(
-          clipBehavior: Clip.none,
-          children: [
-            IconButton(
-              onPressed: () => context.go('/home/notifications'),
-              icon: const _NotificationSvgIcon(),
-            ),
-            if (state.hasUnread)
-              Positioned(
-                right: 8,
-                top: 8,
-                child: Container(
-                  width: 8,
-                  height: 8,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.red,
-                  ),
-                ),
-              ),
-          ],
-        );
-      },
-    );
-  }
-}
-
-class _NotificationSvgIcon extends StatelessWidget {
-  const _NotificationSvgIcon();
-
-  @override
-  Widget build(BuildContext context) {
-    return SvgPicture.asset(
-      AssetPaths.notificationIcon,
-      width: 24,
-      height: 24,
-      colorFilter: const ColorFilter.mode(
-        Colors.white,
-        BlendMode.srcIn,
+Widget _notificationIconButton() {
+  return Builder(
+    builder: (context) => IconButton(
+      onPressed: () => context.go('/home/notifications'),
+      icon: SvgPicture.asset(
+        AssetPaths.notificationIcon,
+        width: 24,
+        height: 24,
+        colorFilter: const ColorFilter.mode(
+          Colors.white,
+          BlendMode.srcIn,
+        ),
       ),
-    );
-  }
+    ),
+  );
 }
